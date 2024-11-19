3de8472e57b29f76ad56cfa149b0e9265910a168
JIDE-73 <167656483+JIDE-73@users.noreply.github.com>
2024-11-19 08:53:48 -0800

    Boton integrantes con redireccion, react router integrado

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,53 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import Main from "./Components/FormMain";
-import dev from "./Components/Integrantes";
+import Integrantes from "./Components/Integrantes";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  return (
+    <header className="bg-gray-800 py-3 text-gray-400">
+      <div className="max-w-4xl mx-auto flex items-center justify-between px-4">
+        <h1 className="text-center text-lg font-bold uppercase flex-1">
+          Calculadoras
+        </h1>
+        <button
+          type="button"
+          onClick={() => navigate("/integrantes")}
+          className="text-white bg-slate-700 hover:text-indigo-950 p-3 rounded hover:bg-slate-500 cursor-pointer border border-indigo-900 hover:border-indigo-700"
+        >
+          Desarrolladores
+        </button>
+      </div>
+    </header>
+  );
+};
 
 const App = () => {
   return (
-    <>
-      <header className="bg-gray-800 py-3 text-gray-400">
-        <div className="max-w-4xl mx-auto flex items-center justify-between px-4">
-          <h1 className="text-center text-lg font-bold uppercase flex-1">
-            Calculadoras
-          </h1>
-          <button
-            type="submit"
-            className="text-white bg-slate-900 hover:text-indigo-950 p-3 rounded hover:bg-slate-500 cursor-pointer"
-          >
-            Desarrolladores
-          </button>
-        </div>
-      </header>
-
-      <section className="py-6 text-gray-700 bg-indigo-900 min-h-screen">
-        <div className="w-full max-w-4xl mx-auto px-4">
-          <Main />
-        </div>
-      </section>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        {/* Ruta raíz que muestra <Main /> */}
+        <Route
+          path="/C_Metdos_num"
+          element={
+            <section className="py-6 text-gray-700 bg-indigo-900 min-h-screen">
+              <div className="w-full max-w-4xl mx-auto px-4">
+                <Main />
+              </div>
+            </section>
+          }
+        />
+        {/* Ruta para el componente <Integrantes /> */}
+        <Route path="/integrantes" element={<Integrantes />} />
+      </Routes>
+    </Router>
   );
 };
 
